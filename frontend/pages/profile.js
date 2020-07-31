02ffd46112befd74289616a8e6a10a9258656e8c
bongjoi <bong5176@gmail.com>
2020-07-31 17:52:35 +0900

    page와 레이아웃 구성

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import AppLayout from '../components/AppLayout';
 
-const Home = () => {
+const Profile = () => {
   return (
     <AppLayout>
-      <div>Hello, Next!</div>
+      <div>프로필 페이지</div>
     </AppLayout>
   );
 };
 
-export default Home;
+export default Profile;
