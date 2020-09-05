1c6373b70e6e02927bf59a561cf15f1265a2731d
bongjoi <bong5176@gmail.com>
2020-09-05 23:48:08 +0900

    Update: backend production modules

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const path = require('path');
+const hpp = require('hpp');
+const helmet = require('helmet');
 const db = require('./models');
 const passportConfig = require('./passport');
 
@@ -22,7 +24,14 @@ db.sequelize
 
 passportConfig();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan('dev'));
+}
+
 app.use(
   cors({
     origin: 'http://localhost:3000',
